9f75e117eb07059608ba941e8c04a298ea78a20e
gbo96 <gbenothmen2@gmail.com>
2019-12-03 03:15:32 +0100

    loader and style

--- app/js/lpc-lazy-background.directive.js
+++ app/js/lpc-lazy-background.directive.js
@@ -4,8 +4,20 @@ angular.module("LpcWebTest2018").directive("lpcLazyBackground", [
   function() {
     var lpcLazyBackgroundLink = function(scope, elem, attr) {
       //TODO
-      //console.log(scope);
+      // create loader
+      let loader = document.createElement("img");
+      loader.src = "/app/img/loading.svg";
+      loader.className = "pot-item-img-loader";
+      elem[0].appendChild(loader);
+
+      // create img html
       let node = document.createElement("img");
+      // remove loader when image is fully loaded!
+      node.onload = function() {
+        loader.remove();
+      };
+
+      // add src to image
       node.src = scope["lpcLazyBackground"];
       node.className = "pot-item-img";
       elem[0].appendChild(node);
